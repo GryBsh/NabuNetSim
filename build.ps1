ba2d22d6462eefba28d3108cd517dc96bcfeffc8
Nick Daniels <nick@lgcstp.co>
2023-04-29 13:45:49 -0400

    One more time

--- build.ps1
+++
@@ -1,4 +0,0 @@
-Remove-Item -Path (Join-Path $PSScriptRoot bin)
-
-#dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -o bin/linux -c release
-dotnet publish ./Nabu.NetSim -r win-x64 -p:PublishSingleFile=true --self-contained true -o bin/windows -c release
